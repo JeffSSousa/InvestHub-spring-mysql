cda5a2de6500e6f8cf38de7c48a86ca1e9ef60d2
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-07-04 15:16:56 -0300

    feat: create project and create properties

---
+++ src/main/java/com/jeffersonssousa/investHub/InvestHubApplication.java
@@ -0,0 +1,13 @@
+package com.jeffersonssousa.investHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestHubApplication.class, args);
+	}
+
+}
