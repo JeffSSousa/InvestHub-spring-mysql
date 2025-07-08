e3c33a31335c8b0f82ba5c86630db277e4c3eb10
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-07-08 00:09:54 -0300

    test: add getUserById and exception tests

--- src/test/java/com/jeffersonssousa/investHub/services/UserServiceTest.java
+++ src/test/java/com/jeffersonssousa/investHub/services/UserServiceTest.java
@@ -7,6 +7,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 
+import java.time.Instant;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -20,6 +23,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.jeffersonssousa.investHub.dto.UserDTO;
 import com.jeffersonssousa.investHub.entities.User;
 import com.jeffersonssousa.investHub.repository.UserRepository;
+import com.jeffersonssousa.investHub.services.exceptions.ControllerNotFoundException;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -76,5 +80,43 @@ public class UserServiceTest {
 		}
 	}
 	
+	
+	@Nested
+	class getUserById{
+		
+		@Test
+		@DisplayName("Should get user by id with success when optional is present")
+		void shouldGetUserByIdWithSuccessWhenOptionaIsPresent() {
+			
+			//Arrange
+			Long id = 1L;
+			User user = new User(id, "Username", "test@email.com", "password", Instant.now(), null);
+			
+			doReturn(Optional.of(user)).when(userRepository).findById(id);
+			
+			//act
+			User output = userService.getUserById(id);
+			
+			//assert
+			assertNotNull(output);
+			assertEquals(user.getUserId(), output.getUserId());
+			assertEquals(user.getEmail(), output.getEmail());
+			assertEquals(user.getPassword(), output.getPassword());
+			assertEquals(user.getCriationTimestamp(), output.getCriationTimestamp());
+		}
+		
+		@Test
+		@DisplayName("Should throw exception when user not found by id")
+		void shouldThrowExceptionWhenUserNotFoundById() {
+			
+			//Arrange
+			Long id = 1L;
+			doReturn(Optional.empty()).when(userRepository).findById(id);
+
+			//act & assert
+			assertThrows(ControllerNotFoundException.class, () -> userService.getUserById(id));
+			
+		}
+	}
 
 }
